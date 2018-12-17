618efc3f25fd54b5a0855c7f1f993a393160e048
Listen <listenzz@163.com>
2018-12-17 18:55:23 +0800

    remove unnecessary files

---
+++ Example/Pods/Target Support Files/HBDStatusBar/HBDStatusBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HBDStatusBar : NSObject
+@end
+@implementation PodsDummy_HBDStatusBar
+@end
---
+++ Example/Pods/Target Support Files/Pods-HBDStatusBar_Example/Pods-HBDStatusBar_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HBDStatusBar_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HBDStatusBar_Example
+@end
